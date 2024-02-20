d4c9492117e2bcba16e6743582d5eeba53f22dfe
sseang <142060734+sseang@users.noreply.github.com>
2024-02-19 19:02:17 -0600

    created POST route for search

--- server/routes/category.router.js
+++ server/routes/category.router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const pool = require('../modules/pool');
+const axios = require('axios');
 
 const router = express.Router();
 
@@ -7,6 +8,10 @@ const API_KEY = process.env.API_KEY;
 
 console.log('API_KEY:', API_KEY);
 
+//url for endpoint
+const BASE_URL_GIFS = 'api.giphy.com/v1/gifs/search';
+
+//GET for DOM render??
 router.get('/', (req, res) => {
   const queryText = `
     SELECT * FROM "categories"
@@ -19,6 +24,32 @@ router.get('/', (req, res) => {
     })
     .catch((error) => {
       console.log(`Error on query ${error}`);
+      res.sendStatus(500);
+    });
+});
+
+//POST route for search endpoint
+router.post('/search', (req, res) => {
+  const { searchAPI } = req.body;
+  // axios call and params
+  axios
+    //query
+    .get(`https://${BASE_URL_GIFS}/?api_key=${API_KEY}&q=${searchAPI}&limit=15`)
+    //success response
+    .then((gifsResponse) => {
+      console.log('SUCCESS', gifsResponse.data);
+      const apiResults = gifsResponse.data.data.map((imageData) => {
+        return {
+          id: imageData.id,
+          url: imageData.images.original.url,
+          alt: imageData.title,
+        };
+      });
+      res.send(apiResults);
+    })
+    //catch errors
+    .catch((error) => {
+      console.log('OH NO!!!:', error);
       res.sendStatus(500);
     });
 });
