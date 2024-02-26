d7ba4672073c154fcf9d35bb4390d3971d3530be
Daniel Holt <holt.daniel.b@gmail.com>
2024-02-25 23:29:50 -0500

    router updated

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const pool = require('../modules/pool');
+const axios = require('axios');
 
 const router = express.Router();
 
 // return all favorite images
-router.get('/', (req, res) => {
-  res.sendStatus(200);
+router.get('/favorites', (req, res) => {
+  const queryText = `
+    SELECT * FROM "favorites"
+      ORDER BY "name" ASC;
+  `;
+  pool
+    .query(queryText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log(`Error on query ${error}`);
+      res.sendStatus(500);
+    });
 });
 
 // add a new favorite
--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -2,15 +2,14 @@ import CategoryMenu from '../CategoryMenu/CategoryMenu';
 
 
 function Favorites() {
+
+
   return (
     <div>
       <p>This is where the favorites go</p>
       <CategoryMenu />
     </div>
   )
-// create favorite button and add image to favorite db
-
-// categories are mutually exclusive
 
 
 }
